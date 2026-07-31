809ac90cfde4d1cc8fed16113302c36af7ec3b03
jordicido <jordi.cidoncha4@gmail.com>
2026-07-31 09:19:55 +0200

    dias 38, 39 y 40

--- frontend/src/components/Message.tsx
+++
@@ -1,9 +0,0 @@
-export function Message({
-  type,
-  children
-}: {
-  type: "success" | "error" | "info";
-  children: React.ReactNode;
-}) {
-  return <div className={`message ${type}`} role="status">{children}</div>;
-}
